1c224eaae62eb351c34c93cd08ad5d0370a51657
Manikanta <manidhruva123@gmail.com>
2025-01-28 19:04:04 +0530

    extracting the object values from an given object

--- PrintObjectValues.js
+++ PrintObjectValues.js
@@ -1,16 +1,18 @@
 //Write a function to print all the values of a given object
 
-//Created the employee object using the const keyword and assigned the values for it. 
-const employee ={
-    Name : "Manikanta",
-    Age : 24,
+function printObjectValues() {
+  const employee = {
+    Name: "Manikanta",
+    Age: 24,
     Designation: "Web-developer",
-    Experience: 3
-};
+    Experience: 3,
+  };
 
-//extracting the object values using "for in" loop method.
+  //extracting the object values using "for in" loop method.
 
-for (let key in employee){
+  for (let key in employee) {
     console.log(employee[key]);
+  }
 }
 
+printObjectValues();
